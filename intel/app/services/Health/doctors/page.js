44539920c2e10a9f69f111554285c7878a816afd
hurshikesh sahu <hurshikeshsahu@hurshikeshs-MacBook-Pro.local>
2024-06-08 17:37:30 +0530

    doctor page design

--- intel/app/services/Health/doctors/page.js
+++ intel/app/services/Health/doctors/page.js
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCar, faBicycle, faWalking, faPhone, faClock, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faCar, faBicycle, faWalking, faPhone, faClock, faStar,faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const FindDoctorPage = () => {
   const [location, setLocation] = useState('');
@@ -9,6 +9,8 @@ const FindDoctorPage = () => {
   const [loading, setLoading] = useState(false);
   const [userCoords, setUserCoords] = useState({ lat: null, lon: null });
   const [errorMessage, setErrorMessage] = useState('');
+  const [sortOption, setSortOption] = useState('distance'); // Default sort option
+  const [showResults, setShowResults] = useState(false);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -46,6 +48,14 @@ const FindDoctorPage = () => {
         `https://discover.search.hereapi.com/v1/discover?at=${userCoordinates.lat},${userCoordinates.lon}&q=doctor&apiKey=smQYaHs6kqHnMongUhEHKnBIXpmilQacnaE9xDCSFYY`
       );
       const data = await response.json();
+      const patientsTellUsOptions = [
+        "Offers Telehealth",
+        "Easy scheduling",
+        "Employs friendly staff",
+        "Accepts walk-ins",
+        "Provides online prescription refill",
+        "Friendly bedside manner"
+      ];
       const doctorsWithDistances = data.items.map(doctor => ({
         ...doctor,
         distance: calculateDistance(userCoordinates.lat, userCoordinates.lon, doctor.position.lat, doctor.position.lng),
@@ -53,17 +63,34 @@ const FindDoctorPage = () => {
         rating: Math.floor(Math.random() * 5) + 1, // Random rating between 1 and 5
         reviews: Math.floor(Math.random() * 1000) + 1,
         isOpenNow: checkIfOpenNow(),
-        formattedOpeningHours: '10:00 - 22:00' // Fixed opening hours
+        formattedOpeningHours: '10:00 - 22:00', // Fixed opening hours
+        isFavorite: Math.random() < 0.5, // Randomly decide if a doctor is a "Patient Favorite"
+        // Selecting 3 random "Patient Tell Us" statements for each doctor
+        patientsTellUs: Array.from(new Set(Array.from({ length: 3 }, () => patientsTellUsOptions[Math.floor(Math.random() * patientsTellUsOptions.length)])))
       }));
-      
-      doctorsWithDistances.sort((a, b) => a.distance - b.distance);
-      
-      setDoctors(doctorsWithDistances);
+
+      setDoctors(sortDoctors(doctorsWithDistances, sortOption));
+      setShowResults(true); // Display results after fetching
     } catch (error) {
       console.error('Error fetching doctors:', error);
       setErrorMessage('Error fetching doctors.');
     }
     setLoading(false);
+  };
+
+  const sortDoctors = (doctors, option) => {
+    switch (option) {
+      case 'distance':
+        return doctors.sort((a, b) => a.distance - b.distance);
+      case 'rating':
+        return doctors.sort((a, b) => b.rating - a.rating);
+      case 'open':
+        return doctors.sort((a, b) => b.isOpenNow - a.isOpenNow);
+      case 'patientFavorite':
+        return doctors.sort((a, b) => b.isFavorite - a.isFavorite);
+      default:
+        return doctors;
+    }
   };
 
   const checkIfOpenNow = () => {
@@ -84,6 +111,12 @@ const FindDoctorPage = () => {
     } else {
       setDoctors([]); // Clear previous doctor data
     }
+  };
+
+  const handleSortChange = (e) => {
+    const newSortOption = e.target.value;
+    setSortOption(newSortOption);
+    setDoctors(sortDoctors([...doctors], newSortOption));
   };
 
   const calculateDistance = (lat1, lon1, lat2, lon2) => {
@@ -116,13 +149,19 @@ const FindDoctorPage = () => {
   const toRad = (value) => (value * Math.PI) / 180;
 
   return (
-    <div className="min-h-screen">
-      <main className="container mx-auto px-4 py-8">
+    <div className="min-h-screen bg-gray-100">
+      <main className="container mx-auto px-4 py-8 ">
         <section className="mb-12">
           <h2 className="text-3xl font-bold mb-6 text-center text-blue-600">Search for Doctors Near You</h2>
           <form onSubmit={handleSearch} className="max-w-lg mx-auto bg-white p-6 rounded-lg shadow-md">
             <div className="mb-4">
-              <label htmlFor="location" className="block text-gray-700 font-bold mb-2">Enter your location (detailed address):</label>
+              <label htmlFor="location" className=" text-gray-700 font-bold mb-2 flex items-center">
+                <span className="mr-2">
+                  <FontAwesomeIcon icon={faMapMarkerAlt} className="text-gray-500" />
+                </span>
+                Enter your location (detailed address):
+              </label>
+
               <input
                 type="text"
                 id="location"
@@ -139,54 +178,90 @@ const FindDoctorPage = () => {
           </form>
         </section>
 
-        {loading ? (
-          <div className="text-center">Loading...</div>
-        ) : errorMessage ? (
-          <div className="text-center text-red-600">{errorMessage}</div>
-        ) : (
-          <section className="mb-12">
-            <h2 className="text-3xl font-bold mb-6 text-center text-blue-600">Doctors Near You</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {doctors.map((doctor) => (
-                <div key={doctor.id} className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300">
-                  <div className="p-6">
-                    <h3 className="text-xl font-semibold text-gray-800 mb-2">{doctor.title}</h3>
-                    <p className="text-gray-600 mb-4">{doctor.address.label}</p>
-                    {doctor.contacts && doctor.contacts[0].mobile && (
-                      <p className="text-gray-800 mb-2 text-xl">
-                        <FontAwesomeIcon icon={faPhone} /> <strong>{doctor.contacts[0].mobile[0].value}</strong>
-                      </p>
-                    )}
-                    {doctor.distance && (
-                      <p className="text-gray-800 mb-2 text-xl">{`Distance: ${doctor.distance.toFixed(2)} km`}</p>
-                    )}
-                    {doctor.travelTime && (
-                      <div className="flex justify-around text-gray-600 mb-2">
-                        <span><FontAwesomeIcon icon={faCar} /> {` ${doctor.travelTime.car.toFixed(0)} min`}</span>
-                        <span><FontAwesomeIcon icon={faBicycle} /> {` ${doctor.travelTime.bike.toFixed(0)} min`}</span>
-                        <span><FontAwesomeIcon icon={faWalking} /> {` ${doctor.travelTime.walk.toFixed(0)} min`}</span>
-                      </div>
-                    )}
-                    <div className="flex items-center">
-                      {[...Array(doctor.rating)].map((_, i) => (
-                        <FontAwesomeIcon key={i} icon={faStar} className="text-yellow-500 mr-1" />
-                      ))}
-                      {[...Array(5 - doctor.rating)].map((_, i) => (
-                        <FontAwesomeIcon key={i} icon={faStar} className="text-gray-300 mr-1" />
-                      ))}
-                        <span className="ml-2 text-gray-700">({doctor.reviews} reviews)</span>
-                      </div>
-                    <div className="text-gray-600 mb-2">
-                      <FontAwesomeIcon icon={faClock} /> {doctor.formattedOpeningHours}
-                    </div>
-                    <p className={`text-lg font-bold ${doctor.isOpenNow ? 'text-green-600' : 'text-red-600'}`}>
-                      {doctor.isOpenNow ? 'OPEN NOW' : 'CLOSED'}
-                    </p>
+        {showResults && (
+          <div>
+            <section className="mb-12 absolute top-80"> 
+              <div className="max-w-lg mx-auto flex justify-between items-center">
+                <label htmlFor="sortOption" className="block text-gray-700 font-bold">Sort by:</label>
+                <div className="relative">
+                  <select id="sortOption" value={sortOption} onChange={handleSortChange} className="border border-gray-300 text-black p-3 rounded-lg pl-8 pr-4 appearance-none">
+                    <option value="distance">Distance</option>
+                    <option value="rating">Rating</option>
+                    <option value="open">Open Now</option>
+                    <option value="patientFavorite">Patient Favorite</option>
+                  </select>
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
+                    <svg className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4"></path>
+                    </svg>
                   </div>
                 </div>
-              ))}
-            </div>
-          </section>
+              </div>
+            </section>
+
+            {loading ? (
+              <div className="text-center">Loading...</div>
+            ) : errorMessage ? (
+              <div className="text-center text-red-600">{errorMessage}</div>
+            ) : (
+              <section className="mb-12">
+                <div className="space-y-8">
+                  {doctors.map((doctor) => (
+                    <div key={doctor.id} className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300 flex">
+                      <img src="https://banner2.cleanpng.com/20180420/jfq/kisspng-general-practitioner-general-surgery-medicine-phys-dr-hat-5ad9e4ae2ae713.3363921815242292941757.jpg" alt="Doctor" className="w-48 h-auto object-cover" />
+                      <div className="p-6 flex-grow">
+                        <h3 className="text-xl font-semibold text-gray-800 mb-2 flex items-center">
+                          {doctor.title}
+                          {doctor.isFavorite && (
+                            <span className="ml-2 px-2 py-1 bg-yellow-300 text-yellow-800 text-xs font-bold rounded">Patient Favorite</span>
+                          )}
+                        </h3>
+                        <p className="text-gray-600 mb-4">{doctor.address.label}</p>
+                        {doctor.contacts && doctor.contacts[0].mobile && (
+                          <p className="text-gray-800 mb-2 text-xl">
+                            <FontAwesomeIcon icon={faPhone} /> <strong>{doctor.contacts[0].mobile[0].value}</strong>
+                          </p>
+                        )}
+                        {doctor.distance && (
+                          <p className="text-gray-800 mb-2 text-xl">{`Distance: ${doctor.distance.toFixed(2)} km`}</p>
+                        )}
+                        {doctor.travelTime && (
+                          <div className="flex justify-around text-gray-600 mb-2">
+                            <span><FontAwesomeIcon icon={faCar} /> {` ${doctor.travelTime.car.toFixed(0)} min`}</span>
+                            <span><FontAwesomeIcon icon={faBicycle} /> {` ${doctor.travelTime.bike.toFixed(0)} min`}</span>
+                            <span><FontAwesomeIcon icon={faWalking} /> {` ${doctor.travelTime.walk.toFixed(0)} min`}</span>
+                          </div>
+                        )}
+                        <div className="flex items-center text-yellow-500 mb-2">
+                          {[...Array(doctor.rating)].map((_, i) => (
+                            <FontAwesomeIcon key={i} icon={faStar} className="mr-1" />
+                          ))}
+                          {[...Array(5 - doctor.rating)].map((_, i) => (
+                            <FontAwesomeIcon key={i} icon={faStar} className="text-gray-300 mr-1" />
+                          ))}
+                          <span className="ml-2 text-gray-700">({doctor.reviews} reviews)</span>
+                        </div>
+                        <div className="text-gray-600 mb-2">
+                          <FontAwesomeIcon icon={faClock} /> {doctor.formattedOpeningHours}
+                        </div>
+                        <p className={`text-lg font-bold ${doctor.isOpenNow ? 'text-green-600' : 'text-red-600'}`}>
+                          {doctor.isOpenNow ? 'OPEN NOW' : 'CLOSED'}
+                        </p>
+                        <div className="mt-4">
+                          <h4 className="text-lg font-semibold text-gray-800 mb-2">Patients Tell Us:</h4>
+                          <ul className="list-disc list-inside">
+                            {doctor.patientsTellUs.map((statement, index) => (
+                              <li key={index} className="text-gray-600">{statement}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </section>
+            )}
+          </div>
         )}
       </main>
     </div>
